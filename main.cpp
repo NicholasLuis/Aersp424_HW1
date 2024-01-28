390b26fcfadadbba6cb64c526b2846ec1aa4c46c
Nicholas Luis <nml5604@psu.edu>
2024-01-28 16:06:49 -0500

    Basic variable initilization & user input

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
+#include <cstdint>
 #include <iostream>
+
+// Made by Nicholas Luis
 
 int main()
 {
-	return 0;
+	// Initializing variables
+	double totalPlaneWeight = 0, totalPlaneMoment = 0;
+	uint16_t frontOccupants, rearOccupants, maxWeight;
+	maxWeight = 2950; // Designed weight limit of the plane (lbs)
+
+	// Requesting user inputs for each of the variables
+	{
+		uint16_t weight = 0;
+		std::cout << "Enter the empty weight of the plane (in lbs): ";
+		std::cin >> weight;
+		totalPlaneWeight += weight;
+	}
+	std::cout<<"The total weight of the plane is: "<<totalPlaneWeight<<std::endl;
 }
