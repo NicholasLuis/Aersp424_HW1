ef5d7c14a62f2a3d76ec3a09611fe1ae1cd3f2aa
Nicholas Luis <nml5604@psu.edu>
2024-02-03 16:56:37 -0500

    Still working on problem 1 of HW

--- main.cpp
+++ main.cpp
@@ -8,23 +8,25 @@
 int main()
 {
 	// Initializing variables
-	double totalPlaneWeight = 0, totalPlaneMoment = 0, fuelGallons = 0;
-	static uint16_t maxWeight = 2950; // design limit of the plane (lbs)
+	double totalPlaneWeight = 0, totalPlaneMoment = 0, fuelGallons = 0, centerOfGravity;
+	static uint16_t maxWeight = 2950; // designed weight limit of the plane (lbs)
+	static long frontCoG = 82.1, aftCoG = 84.7; // designed center of gravity lmit of the plane (inches)
 
-	// This scope calculates the initial moment due to passengers, cargo, and the aircraft structure
+	// This scope calculates the initial moment due to passengers, cargo, fuel, and the aircraft structure
 	{
 		// Intializing temporary variables
 		uint16_t weight, frontOccupantsNumber, rearOccupantsNumber, cargoWeight;
-		double frontOccupantsMoment, frontOccupantsMomentArm, frontOccupantsWeight,
-			   rearOccupantsMoment, rearOccupantsMomentArm, rearOccupantsWeight;
+		double frontOccupantsMoment, frontOccupantsMomentArm, frontOccupantsWeight = 0,
+			rearOccupantsMoment, rearOccupantsMomentArm, rearOccupantsWeight = 0,
+			cargoMomentArm, cargoMoment, fuelWeight, fuelMoment, fuelMomentArm;
 
 		// Requests plane's empty weight, passengers, fuel, and cargo 
-		std::cout << "Enter the empty weight of the plane (in lbs): ";
+		std::cout << "Enter the empty weight of the plane (lbs): ";
 		std::cin >> totalPlaneWeight;
-		std::cout << "Enter the empty-weight moment of the plane (in lb-in): ";
+		std::cout << "Enter the empty-weight moment of the plane (lb-in): ";
 		std::cin >> totalPlaneMoment;
 
-		std::cout << "Enter the total weight of the cargo(in lbs): ";
+		std::cout << "Enter the total weight of the cargo(lbs): ";
 		std::cin >> cargoWeight;
 		std::cout << "Enter the number of occupants in the front seats: ";
 		std::cin >> frontOccupantsNumber;
@@ -32,33 +34,46 @@ int main()
 		std::cin >> rearOccupantsNumber;
 		
 		// Calculates total moment from the front passengers
-		std::cout << "Enter the moment arm of the front seat (in inches): ";
+		std::cout << "Enter the moment arm of the front seat (inches): ";
 		std::cin >> frontOccupantsMoment;
-		for (int i = 0; i < frontOccupantsNumber; i++)
+		std::cout << "Enter the weights (lbs) of the passengers in the front seat. Separate the numbers with spaces: ";
+		for (int i = 0; i < rearOccupantsNumber; i++)
 		{
-			std::cout << "Enter the weight of passenger #" << i << " in the front seat (in lbs): ";
 			std::cin >> weight;
 			frontOccupantsWeight += weight;
 		}
 		frontOccupantsMoment = frontOccupantsNumber * frontOccupantsWeight;
 
 		// Calculates total moment from the rear passengers
-		std::cout << "Enter the moment arm of the rear seat (in inches): ";
+		std::cout << "Enter the moment arm of the rear seat (inches): ";
 		std::cin >> rearOccupantsMoment;
+		std::cout << "Enter the weights (lbs) of the passengers in the rear seat. Separate the numbers with spaces: ";
 		for (int i = 0; i < rearOccupantsNumber; i++)
 		{
-			std::cout << "Enter the weight of passenger #" << i << " in the rear seat (in lbs): ";
 			std::cin >> weight;
 			rearOccupantsWeight += weight;
 		}
-		
 		rearOccupantsMoment = rearOccupantsNumber * rearOccupantsWeight;
+
+		// Calculates total moment from the fuel
+		std::cout << "Enter the number of gallons of usable fuel: ";
+		std::cin >> fuelGallons;
+		std::cout << "Enter the weight of 1 gallon of fuel (lbs): ";
+		std::cin >> cargoMomentArm;
+		std::cout << "Enter the moment arm of the fuel tank (inches): ";
+		std::cin >> cargoWeight;
+
+		// Calculates total moment from the baggage
+		std::cout << "Enter the moment arm of the cargo (inches): ";
+		std::cin >> cargoMomentArm;
+		std::cout << "Enter the weight of the cargo (lbs): ";
+		std::cin >> cargoWeight;
+
+
 		totalPlaneWeight += (frontOccupantsWeight + rearOccupantsWeight);
 		totalPlaneMoment += (frontOccupantsMoment + rearOccupantsMoment);
+		centerOfGravity = totalPlaneMoment / totalPlaneWeight;
 	}
-
-
-
 
 
 
