09a83c953eb2d5004b4c87c5ec71cb3aea91425f
Nicholas Luis <nml5604@psu.edu>
2024-02-04 19:47:23 -0500

    Calculating how much fuel to add/remove

--- main.cpp
+++ main.cpp
@@ -1,14 +1,24 @@
 #include <cstdint>
 #include <iostream>
+#include <iomanip>
 
-// Made by Nicholas Luis
+// HW1 Problem 1 - Made by Nicholas Luis
 // Inputs: weight, number of passengers, moment arms, etc.
 // Output: Amount of fuel to add/remove to balance the plane and stay below the max weight
+// 
+// Note: This program assumes that adding/removing fuel for weight does not significantly
+//		 push the plane to be unbalanced (or vice-versa: balancing does not lead it to exceed the weight limit)
+
+ double fuelChange(double initialMoment, double initialWeight, double perGallonWeight, double fuelMomentArm)
+{
+	return 1.0;
+}
 
 int main()
 {
 	// Initializing variables
-	double totalPlaneWeight = 0, totalPlaneMoment = 0, fuelGallons = 0, fuelWeight = 0, centerOfGravity;
+	double totalPlaneWeight = 0, totalPlaneMoment = 0, centerOfGravity,
+		fuelGallons = 0, perGallonWeight =0, totalFuelWeight = 0, fuelMomentArm = 0;
 	static uint16_t maxWeight = 2950; // designed weight limit of the plane (lbs)
 	static long frontCoG = 82.1, aftCoG = 84.7; // designed center of gravity lmit of the plane (inches)
 
@@ -18,7 +28,7 @@ int main()
 		uint16_t weight, frontOccupantsNumber, rearOccupantsNumber, cargoWeight;
 		double frontOccupantsMoment, frontOccupantsWeight = 0, momentArm,
 			 rearOccupantsMoment, rearOccupantsWeight = 0,
-			 cargoMoment, fuelMoment;
+			 cargoMoment, fuelWeight, fuelMoment;
 
 		// Requests plane's empty weight, passengers, fuel, and cargo 
 		std::cout << "Enter the empty weight of the plane (lbs): ";
@@ -26,68 +36,102 @@ int main()
 		std::cout << "Enter the empty-weight moment of the plane (lb-in): ";
 		std::cin >> totalPlaneMoment;
 
-		std::cout << "Enter the total weight of the cargo(lbs): ";
-		std::cin >> cargoWeight;
-		std::cout << "Enter the number of occupants in the front seats: ";
-		std::cin >> frontOccupantsNumber;
-		std::cout << "Enter the number of occupants in the rear seats: ";
-		std::cin >> rearOccupantsNumber;
-		
 		// Calculates total moment from the front passengers
-		std::cout << "Enter the moment arm of the front seat (inches): ";
-		std::cin >> momentArm;
+		std::cout << "Enter the number of occupants in the front seats: ";
+		std::cin >> frontOccupantsNumber;		
 		std::cout << "Enter the weights (lbs) of each passenger in the front seat. Separate the numbers with spaces: ";
-		for (int i = 0; i < rearOccupantsNumber; i++)
+		for (int i = 0; i < frontOccupantsNumber; i++)
 		{
 			std::cin >> weight;
 			frontOccupantsWeight += weight;
 		}
+		std::cout << "Enter the moment arm of the front seat (inches): ";
+		std::cin >> momentArm;
+
 		frontOccupantsMoment = frontOccupantsWeight * momentArm;
 
 		// Calculates total moment from the rear passengers
-		std::cout << "Enter the moment arm of the rear seat (inches): ";
-		std::cin >> momentArm;
+		std::cout << "Enter the number of occupants in the rear seats: ";
+		std::cin >> rearOccupantsNumber;
 		std::cout << "Enter the weights (lbs) of each passenger in the rear seat. Separate the numbers with spaces: ";
 		for (int i = 0; i < rearOccupantsNumber; i++)
 		{
 			std::cin >> weight;
 			rearOccupantsWeight += weight;
 		}
+		std::cout << "Enter the moment arm of the rear seat (inches): ";
+		std::cin >> momentArm;
 		rearOccupantsMoment = rearOccupantsWeight * momentArm;
 
 		// Calculates total moment from the fuel
 		std::cout << "Enter the number of gallons of usable fuel: ";
 		std::cin >> fuelGallons;
 		std::cout << "Enter the weight of 1 gallon of fuel (lbs): ";
-		std::cin >> weight;
+		std::cin >> perGallonWeight;
 		std::cout << "Enter the moment arm of the fuel tank (inches): ";
 		std::cin >> momentArm;
-		fuelWeight = fuelGallons * weight;
-		fuelMoment = fuelGallons * fuelWeight * momentArm;
+		fuelWeight = fuelGallons * perGallonWeight;
+		fuelMoment = fuelWeight * momentArm;
 
 		// Calculates total moment from the baggage
+		std::cout << "Enter the weight of the baggage (lbs): ";
+		std::cin >> cargoWeight;
 		std::cout << "Enter the moment arm of the cargo (inches): ";
 		std::cin >> momentArm;
-		std::cout << "Enter the weight of the cargo (lbs): ";
-		std::cin >> cargoWeight;
 		cargoMoment = cargoWeight * momentArm;
 
+		// Calculates total weight, total moment, and the center of gravity of the plane
 		totalPlaneWeight += (frontOccupantsWeight + rearOccupantsWeight + fuelWeight + cargoWeight);
 		totalPlaneMoment += (frontOccupantsMoment + rearOccupantsMoment + fuelMoment + cargoMoment);
 		centerOfGravity = totalPlaneMoment / totalPlaneWeight;
 	}
 
-	std::cout << "The total plane weight is: " << totalPlaneWeight << " (lbs)\nThe max weight is " << maxWeight;
-	std::cout << "The total plane CoG is at: " << centerOfGravity << " (inches)\nThe center of gravity should be between " 
-		      << frontCoG << " and " << aftCoG << " (inches)";
+	// This scope determines how much fuel to add/remove (if any)
+	{
+		bool atLimit = false;
+		
+		if (totalPlaneWeight > maxWeight)
+		{
+			std::cout << "\nThe plane is over the weight limit. Remove "
+				<< std::fixed << std::setprecision(2) << ((maxWeight - totalPlaneWeight) / perGallonWeight)
+				<< " gallons of fuel";
+			bool atLimit = true; // The plane is now at the weight limit after removing fuel
+		}
+
+		if (centerOfGravity < frontCoG) // Checks if CoG too far forward
+		{
+
+			// Compares center of gravity to the fuel moment arm to see if we should add or remove fuel
+			if (centerOfGravity < fuelMomentArm) // Need to add fuel to balance
+			{
+				if (atLimit) {
+					std::cout << "\nThe plane is at the weight limit and is unbalanced. Consider moving weight"
+						" from the front to the rear of the aircraft"; 
+				}
+				else
+				{
+					std::cout << "\nRemove " << std::fixed << std::setprecision(2)
+							<< 
+							<< " gallons of fuel to balance the plane's weight";
+				}
 
 
-	//// Adjusts the amount of fuel to make sure the plane is within design limits
-	//bool takeoff = false;
-	//
-	//while (takeoff = false)
-	//{
-	//
-	//
-	//}
+			}
+			else
+			{
+
+			}
+
+		}
+		else if (centerOfGravity > aftCoG) // Checks if CoG too far back
+		{
+			std::cout << "";
+		}
+		else
+		{
+			std::cout << "The plane is within the design limits.";
+		}
+	}
+
+	return 0;
 }
