f3855eb28414f9369d816578f0cbcf0ecf8431b2
Nicholas Luis <nml5604@psu.edu>
2024-02-14 23:09:32 -0500

    Initial setup of Plane class - Finished Question 3

--- Plane.cpp
+++ Plane.cpp
@@ -2,8 +2,19 @@
 #include <iostream>
 #include <array>
 
-Plane::Plane(std::string start, std::string end) : origin(start), destination(end)
+Plane::Plane(std::string start, std::string end)
+	: origin(start), destination(end),
+	pos(0.0), vel(0.0), distance(0.0), at_SCE(false) // initializing values
 {
-	//implementation
-
+	std::cout << "Constructor executed" << std::endl;
+	pos = 0;
 }
+
+// Basic functions that return or change the object's values
+double Plane::getPos() { return pos; }
+double Plane::getVel() { return vel;  }
+void Plane::setVel(double newVel) { vel = newVel; }
+std::string	Plane::getOrigin() { return Plane::origin; }
+std::string	Plane::getDestination() { return destination; }
+void Plane::Operate(double dt) {}
+bool Plane::get_at_SCE() { return at_SCE; }
--- Plane.h
+++ Plane.h
@@ -6,8 +6,8 @@
 
 class Plane {
 private:
-	double pos = 0.0, vel = 0.0, distance = 0.0;
-	bool at_SCE = 0;
+	double pos, vel, distance;
+	bool at_SCE;
 	std::string origin, destination;
 	std::array< std::string, 3> flightList = {
 		"There are 160-mile flights between \"SCE\" and \"PHL\"",
@@ -17,6 +17,15 @@ private:
 public:
 	Plane(std::string start, std::string end); // Constructor that takes in the origin & destination strings}
 	~Plane() { std::cout << "Deconstructor executed<" << std::endl; }; // Destructor
+
+	// Initializing functions
+	double getPos();
+	double getVel();
+	void setVel(double newVel);
+	std::string	getOrigin();
+	std::string	getDestination();
+	void	Operate(double dt);
+	bool	get_at_SCE();
 
 };
 #endif
