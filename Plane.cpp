4a218bf083888b14f919af6d1a3675141664cfc5
Nicholas Luis <nml5604@psu.edu>
2024-02-16 00:08:40 -0500

    Done w/ all but operate fcn in question 4

--- Plane.cpp
+++ Plane.cpp
@@ -4,9 +4,18 @@
 
 Plane::Plane(std::string start, std::string end)
 	: origin(start), destination(end),
-	pos(0.0), vel(0.0), distance(0.0), at_SCE(false) // initializing values
-{
+	pos(0.0), vel(0.0), distance(0.0), at_SCE(0) // initializing values
+{ // Implementations
 	std::cout << "Plane created at: " << this << std::endl;
+	
+	//finds the index of the destination airport and updates the corresponding distance
+	for (int i = 0; i < size(airportNames); i++) 
+	{
+		// Always either starting or ending from SCE. So we only check the other airport name
+		if ( (origin.compare(airportNames[i]) ) || (destination.compare(airportNames[i]) ) )	{
+			distance = airportDistances[i];
+		}
+	}
 }
 
 // Basic functions that return or change the object's values
